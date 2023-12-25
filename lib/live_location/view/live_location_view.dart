3e17e20e14fb5cf9338666585fca5c1f93d245dc
Amit Mondal <androidone548@gmail.com>
2023-12-25 15:39:38 +0530

    Google maps implemented

--- lib/live_location/view/live_location_view.dart
+++ lib/live_location/view/live_location_view.dart
@@ -1,8 +1,25 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:rmpl_hrm_admin/constants/colors.dart';
 
-class LiveLocationView extends StatelessWidget {
+class LiveLocationView extends StatefulWidget {
   const LiveLocationView({super.key});
+
+  @override
+  State<LiveLocationView> createState() => LiveLocationViewState();
+}
+
+class LiveLocationViewState extends State<LiveLocationView> {
+  final Completer<GoogleMapController> _controller =
+      Completer<GoogleMapController>();
+
+  static const CameraPosition _kGooglePlex = CameraPosition(
+    bearing: 192.8334901395799,
+    target: LatLng(37.4219983, -122.084),
+    zoom: 19.151926040649414,
+  );
 
   @override
   Widget build(BuildContext context) {
@@ -16,7 +33,7 @@ class LiveLocationView extends StatelessWidget {
         elevation: 0,
         titleSpacing: 0,
         title: const Text(
-          "Employee's Profile",
+          'User name location',
           style: TextStyle(
             fontFamily: 'Inter',
             fontSize: 18,
@@ -24,6 +41,34 @@ class LiveLocationView extends StatelessWidget {
             color: Colors.white,
           ),
         ),
+      ),
+      body: GoogleMap(
+        mapType: MapType.hybrid,
+        initialCameraPosition: _kGooglePlex,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+        circles: {
+          Circle(
+            circleId: const CircleId('main_circle'),
+            center: const LatLng(37.4219983, -122.084),
+            radius: 20,
+            strokeWidth: 2,
+            fillColor: Colors.blue.withOpacity(0.2),
+            strokeColor: Colors.blue.withOpacity(0.4),
+            zIndex: 2,
+            consumeTapEvents: false,
+          ),
+        },
+        markers: {
+          Marker(
+            markerId: const MarkerId('main_marker'),
+            position: const LatLng(37.4219983, -122.084),
+            icon: BitmapDescriptor.defaultMarkerWithHue(
+              BitmapDescriptor.hueRed,
+            ),
+          ),
+        },
       ),
     );
   }
